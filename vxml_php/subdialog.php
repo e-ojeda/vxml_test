f13f8d95f385cce3513990049bdc6a5814378952
Eduardo Ojeda <eojeda@vailsys.com>
2012-09-27 10:45:45 -0500

    Adding another subdialog example, http get with PHP

---
+++ vxml_php/subdialog.php
@@ -0,0 +1,18 @@
+<?php
+header("Content-type: text/xml");
+echo("<?xml version=\"1.0\"?>\n");
+$random1 = "green";
+$random2 = "blue";
+$random3 = "yellow";
+?>
+
+<vxml version="2.0">
+  <form>
+    <block>
+      <var name="stuff1" expr="'<?php echo($random1)?>'"/>
+      <var name="stuff2" expr="'<?php echo($random2)?>'"/>
+      <var name="stuff3" expr="'<?php echo($random3)?>'"/>
+      <return namelist="stuff1 stuff2 stuff3"/>
+    </block>
+  </form>
+</vxml>
